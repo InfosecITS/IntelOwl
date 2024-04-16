578ef200551cbbb41d28d1ed5bba1bd67cfa1523
ITSinfosec7 <infosec7@syr.edu>
2024-04-16 16:34:58 -0400

    added ufw-docker firewall scriptush

--- ufw-docker.sh
+++ ufw-docker.sh
@@ -15,16 +15,13 @@ apply_firewall_rules() {
   local from_ip=$1
   local container_ip=$2
   local port=$3
+ 
+  echo "Applying firewall rules for $from_ip to access $container_ip on port $port"
+  
 
-  # Check if IP is within the expected subnet (optional, for safety)
-  if [[ $container_ip =~ ^172\ ]]; then
-    echo "Applying firewall rules for $from_ip to access $container_ip on port $port"
-    
     # Add new rule
-    sudo ufw route allow proto tcp from $from_ip to $container_ip port $port
-  else
-    echo "Error: Container IP ($container_ip) is not within the expected subnet."
-  fi
+   sudo ufw route allow proto tcp from $from_ip to $container_ip port $port
+  
 }
 
 # Loop through each external IP and apply firewall rules
