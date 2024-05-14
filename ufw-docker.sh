da014c4324608015cd164edef250a8fbea9aed2e
root <root@its-sec-misp.ad.syr.edu>
2024-05-14 12:35:55 -0400

    updated default.yaml

--- ufw-docker.sh
+++ ufw-docker.sh
@@ -5,26 +5,41 @@ container_name="intelowl_nginx"
 # Define the array of external IPs that need access
 external_ips=("128.230.49.121" "128.230.67.26") # Add more IPs as needed
 # Define the port you want to allow access to
-port="443"
+port="8080"
 
-# Fetch the current container IP
-container_ip=$(sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_name)
+# Function to delete existing firewall rules
+delete_firewall_rules() {
+  local from_ip=$1
+  local container_ip=$2
+  local port=$3
+
+  echo "Deleting existing firewall rules for $from_ip accessing $container_ip on port $port"
+
+  # Delete existing rule if it exists
+  sudo ufw route delete proto tcp from $from_ip to $container_ip port $port
+}
 
 # Function to apply firewall rules
 apply_firewall_rules() {
   local from_ip=$1
   local container_ip=$2
   local port=$3
- 
-  echo "Applying firewall rules for $from_ip to access $container_ip on port $port"
-  
 
-    # Add new rule
-   sudo ufw route allow proto tcp from $from_ip to $container_ip port $port
-  
+  echo "Applying firewall rules for $from_ip to access $container_ip on port $port"
+
+  # Add new rule
+  sudo ufw route allow proto tcp from $from_ip to $container_ip port $port
 }
 
-# Loop through each external IP and apply firewall rules
+# Fetch the current container IP
+container_ip=$(sudo docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_name)
+
+# Loop through each external IP and delete existing firewall rules
+for from_ip in "${external_ips[@]}"; do
+  delete_firewall_rules $from_ip $container_ip $port
+done
+
+# Loop through each external IP and apply new firewall rules
 for from_ip in "${external_ips[@]}"; do
   apply_firewall_rules $from_ip $container_ip $port
 done
