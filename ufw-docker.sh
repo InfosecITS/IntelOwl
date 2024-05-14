38638b42626b6d23e53b6a19ee1d9be6e5287dd3
root <root@its-sec-misp.ad.syr.edu>
2024-05-14 17:00:02 -0400

    updated default.yaml

--- ufw-docker.sh
+++ ufw-docker.sh
@@ -5,7 +5,7 @@ container_name="intelowl_nginx"
 # Define the array of external IPs that need access
 external_ips=("128.230.49.121" "128.230.67.26") # Add more IPs as needed
 # Define the port you want to allow access to
-port="80"
+port="8443"
 
 # Function to delete existing firewall rules
 delete_firewall_rules() {
@@ -16,7 +16,7 @@ delete_firewall_rules() {
   echo "Deleting existing firewall rules for $from_ip accessing $container_ip on port $port"
 
   # Delete existing rule if it exists
-  sudo ufw route delete proto tcp from $from_ip to $container_ip port $port
+  #sudo ufw route delete proto tcp from $from_ip to $container_ip port $port
 }
 
 # Function to apply firewall rules
