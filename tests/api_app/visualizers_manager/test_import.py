637723087ac34a805d3562741313bc8aa0554c91
root <root@its-sec-misp.ad.syr.edu>
2024-06-12 16:51:04 -0400

    djamgo

--- tests/api_app/visualizers_manager/test_import.py
+++
@@ -1,7 +0,0 @@
-from api_app.visualizers_manager.visualizers.all_ip_visualizers import IPReputationServices
-
-# Example usage
-ip_service = IPReputationServices()
-ip_to_check = "192.168.1.0"
-ip_service.check_ip_reputation(ip_to_check)
-ip_service.visualize_reputation(ip_to_check)
